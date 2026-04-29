0aa8c565b93a72ea4604af94a31271461ea78e8e
IgorAntunes66 <igorgantunes@hotmail.com>
2026-04-29 10:34:19 -0400

    finish `/api/v1/status` endpoint

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,15 @@ async function query(queryObject) {
         password: process.env.POSTGRES_PASSWORD,
         port: process.env.POSTGRES_PORT
     })
-    await client.connect()
-    const result = await client.query(queryObject)
-    await client.end()
-    return result
+    try {
+        await client.connect()
+        const result = await client.query(queryObject)
+        return result
+    } catch (error) {
+        console.error(error)
+    } finally {
+        await client.end()
+    }
 }
 
 export default {
